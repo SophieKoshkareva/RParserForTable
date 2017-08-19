021ee062a03679880ecf902c5c754bce51becaf0
Runia_ <sofysilchenko@gmail.com>
2017-08-19 18:40:49 +0700

    Method FindMissingValue  works for class MissingValue works

--- MissingValue.R
+++ MissingValue.R
@@ -5,32 +5,34 @@ MissingValue <- setClass("MissingValue",
 setMethod(f = "initialize",
           signature = "MissingValue",
           definition = function(.Object)
-          { .Object@style <- CellStyle(wb) +
-            Font(wb, isItalic = TRUE) +
-            Fill(foregroundColor = "lightgray") +
-            Border(position = c("BOTTOM", "LEFT", "TOP", "RIGHT"))
+          { .Object@style <- "gray"
+            # .Object@style <- CellStyle(wb) +
+            # Font(wb, isItalic = TRUE) +
+            # Fill(foregroundColor = "lightgray") +
+            # Border(position = c("BOTTOM", "LEFT", "TOP", "RIGHT"))
           return(.Object)
           }
 )
 
-FindMissingValue <- setGeneric(name = "FindMissingValue",
+setGeneric(name = "FindMissingValue",
                                def = function(theObject, data_table)
                                { 
                                  standardGeneric("FindMissingValue")
                                 }
 )
 
-FindMissingValue <- setMethod(f = "FindMissingValue",
+setMethod(f = "FindMissingValue",
                               signature = "MissingValue",
                               definition = function(theObject, data_table)
                               { 
                                 mis_ind <- which(is.na(data_table), arr.ind = TRUE, useNames = FALSE)
-                                # if (is.null(mis_ind) == TRUE) {
-                                #   print("No missing values")
-                                #  # xlsx.createBook(x, sheet_out_name, file_out)
-                                # } else {
-                                #   mis_ind[,1] <- mis_ind[,1] + row_header + row_symbol
+                                if (is.null(mis_ind) == TRUE) {
+                                  print("No missing values")
+                                 # xlsx.createBook(x, sheet_out_name, file_out)
+                                } else {
+                                  mis_ind[,1] <- mis_ind[,1] + theObject@row_header + theObject@row_symbol
                                   theObject@ind <- apply(mis_ind, 1, paste, collapse = ".")
-                                #}
+                                }
+                                return(theObject)
                               }
 )
