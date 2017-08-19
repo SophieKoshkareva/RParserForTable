5e86b0fd9a8ef70777d774c89bf649cca114193c
Runia_ <sofysilchenko@gmail.com>
2017-08-20 01:50:08 +0700

    print() was removed in the method FindMissingValue

--- MissingValue.R
+++ MissingValue.R
@@ -25,20 +25,13 @@ setMethod(f = "FindMissingValue",
           signature = "MissingValue",
           definition = function(theObject, table, column_index)
           { mis_row_ind <- 0
-            
-            print(a <- which(is.na(table[column_index])))
-            print(mis_row_ind)
             mis_row_ind <- which(is.na(table[column_index]))
-            #mis_row_ind <- which(is.na(table[column_index]), arr.ind = TRUE, useNames = FALSE)
-            print(mis_row_ind)
             if (is.null(mis_row_ind) == TRUE) {
               print("No missing values")
             # xlsx.createBook(x, sheet_out_name, file_out)
             } else {
               mis_row_ind <- mis_row_ind + theObject@row_header + theObject@row_symbol
-              print(mis_row_ind)
               theObject@ind <- append(theObject@ind, values = outer(mis_row_ind, column_index, paste, sep = "."))
-              #theObject@ind <- apply(mis_row_ind, 1, paste, collapse = ".")
             }
             return(theObject)
           }
