cf18f8b9f6737cd19b3aa290ea0f8fc4180c2de5
Runia_ <sofysilchenko@gmail.com>
2017-08-22 20:00:53 +0700

    Starting to write method FindMisprints for numeric variables

--- Misprint.R
+++ Misprint.R
@@ -24,60 +24,58 @@ setGeneric(name = "FindMisprints",
 )
 
 setMethod(f = "FindMisprints",
-          signature = "Misprint",
-          definition = function(theObject, table, column_index, keys, values)
-          { 
-            #misprint_row_ind <- 0
-            c <- table[[column_index]]
-            
-            for (i in 1:length(c))
-            { 
-              found <- FALSE
-              if (is.na(c[i]) == TRUE)
-              {
-                globalMissing <<- append(globalMissing, values = paste(i, column_index, sep = "."))
-                cat("Missing value coordinates are ", paste(i, column_index, sep = "."), "\n")
-                next
-              }
-              
-              for(j in 1:length(values))
-              {
-                if (toupper(c[i]) == toupper(values[[j]][1]))
-                {
-                  #which(toupper(file@table[[2]]) %in% toupper(sex@key[[1]][1]))
-                  found <- TRUE
-                  break
-                }
-              }
-              
-              if (found)
-                next
-              
-              for(a in 1:length(keys))
-              { print(length(keys))
-                if (found)
-                  break
-                for(b in 1:length(keys[[a]]))
-                { print(c(b, length(keys[[a]])))
-                  if (toupper(c[i]) == toupper(keys[[a]][b]))
-                  {
-                    found <- TRUE
-                    #misprint_row_ind <-  i + theObject@row_header + theObject@row_symbol
-                    theObject@indices <- append(theObject@indices, values = paste(i, column_index, sep = "."))
-                    cat("Misprints coordinates are ", paste(i, column_index, sep = "."), "\n")
-                    break
-                  }
-                  
-                  #next
-                }
-                if ((!found) & (a == length(keys)))
-                {
-                  globalUnsolvedMisprint <<- append(globalUnsolvedMisprint, values = paste(i, column_index, sep = "."))
-                  cat("Unsolved misprint coordinates are", paste(i, column_index, sep = "."), "\n")
-                  found <- TRUE
-                }
-              }
-            }
-            return(theObject)
-          }       
+  signature = "Misprint",
+  definition = function(theObject, table, column_index, keys, values)
+  { 
+    c <- table[[column_index]]
+    
+    for (i in 1:length(c))
+    { 
+      found <- FALSE
+      if (is.na(c[i]) == TRUE)
+      {
+        globalMissing <<- append(globalMissing, values = paste(i, column_index, sep = "."))
+        cat("Missing value coordinates are ", paste(i, column_index, sep = "."), "\n")
+        next
+      }
+      
+      for(j in 1:length(values))
+      {
+        if (toupper(c[i]) == toupper(values[[j]][1]))
+        {
+          #which(toupper(file@table[[2]]) %in% toupper(sex@key[[1]][1]))
+          found <- TRUE
+          break
+        }
+      }
+      
+      if (found)
+        next
+      
+      for(a in 1:length(keys))
+      { 
+        if (found)
+          break
+        for(b in 1:length(keys[[a]]))
+        { 
+          if (toupper(c[i]) == toupper(keys[[a]][b]))
+          {
+            found <- TRUE
+            #misprint_row_ind <-  i + theObject@row_header + theObject@row_symbol
+            globalMisprint <- append(globalMisprint, values = paste(i, column_index, sep = "."))
+            #theObject@indices <- append(theObject@indices, values = paste(i, column_index, sep = "."))
+            cat("Misprints coordinates are ", paste(i, column_index, sep = "."), "\n")
+            break
+          }
+        }
+        if ((!found) & (a == length(keys)))
+        {
+          globalUnsolvedMisprint <<- append(globalUnsolvedMisprint, values = paste(i, column_index, sep = "."))
+          cat("Unsolved misprint coordinates are", paste(i, column_index, sep = "."), "\n")
+          found <- TRUE
+        }
+      }
+    }
+    #return(theObject)
+  }       
 )
