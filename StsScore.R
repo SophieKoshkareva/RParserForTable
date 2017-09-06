3ee5286f58da276f1648a1ec1b4a81ea47f24ab9
Runia_ <sofysilchenko@gmail.com>
2017-09-07 00:33:03 +0700

    new class StsScore

---
+++ StsScore.R
@@ -0,0 +1,29 @@
+StsScore <- setClass("StsScore", 
+                contains = "Column"
+)
+
+setMethod(f = "initialize",
+          signature = "StsScore",
+          definition = function(.Object)
+          { 
+            .Object@column_index <- 13
+            return(.Object)
+          }
+)
+
+setGeneric(name = "FindErrors", 
+           def = function(theObject)
+           {
+             standardGeneric("FindErrors")
+           }
+)
+
+setMethod(f = "FindErrors",
+          signature = "StsScore",
+          definition = function(theObject)
+          { 
+            #FindMisprints(file@table_in, theObject@column_index, theObject@key, theObject@value)
+            FindMisprintsForNumeric(misprint, file@table_in, theObject@column_index, file@row_header, file@row_table_legend)
+            FindOutliers(outlier, file@table_in, theObject@column_index, file@row_header, file@row_table_legend)
+          }
+)
