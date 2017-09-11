123afc1cf0209c849301a591100f3f7b534be213
Runia_ <sofysilchenko@gmail.com>
2017-09-12 02:18:32 +0700

    year refactored to 2 or 4 simbols

--- Pattern.R
+++ Pattern.R
@@ -1,4 +1,9 @@
 string <- "25.12.1994"
-string1 <- "1234"
-pattern <- "^(\\d){4}$"
+string1 <- "11122"
+pattern <- "^((\\d){2}|(\\d){4})$"
+grepl(pattern, string1)
+
+
+string1 <- "12."
+pattern <- "^(\\d){1,2}[/-]$"
 grepl(pattern, string1)
